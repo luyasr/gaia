cfbdc2393a9877ae1068ebd146087364281c9aa2
luyasanr <luyasanr@gmail.com>
2023-11-21 23:58:32 +0800

    fix config

--- config/config.go
+++ config/config.go
@@ -12,31 +12,18 @@ import (
 var C = new(config)
 
 type Config interface {
-	Load(string) *config
-	Watch()
-	Merge(any) error
+	Load(string, any) *config
+	Watch() *config
 }
 
 type config struct {
+	cfg any
 }
 
-func pathParse(path string) (string, string, string) {
-	dir := filepath.Dir(path)
-	filename := filepath.Base(path)
-	extension := filepath.Ext(filename)
-	fileType := strings.TrimPrefix(extension, ".")
-	filenameOnly := strings.TrimSuffix(filename, extension)
+// Load loads the config from the given path.
+func (c *config) Load(path string, cfgObj any) *config {
+	c.cfg = cfgObj
 
-	return dir, filenameOnly, fileType
-}
-
-func handleError(err error, message string) {
-	if err != nil {
-		log.Fatal(errors.Internal(message, err.Error()))
-	}
-}
-
-func (c *config) Load(path string) *config {
 	dir, filenameOnly, extension := pathParse(path)
 	viper.AddConfigPath(dir)
 	viper.SetConfigName(filenameOnly)
@@ -49,24 +36,41 @@ func (c *config) Load(path string) *config {
 		handleError(err, "read conf failed")
 	}
 
-	if err := viper.Unmarshal(C); err != nil {
+	if err := viper.Unmarshal(&c.cfg); err != nil {
 		handleError(err, "unmarshal conf failed")
 	}
 
-	return C
+	return c
 }
 
-func (c *config) Watch() {
+// Watch watches the config file.
+func (c *config) Watch() *config {
 	viper.OnConfigChange(func(e fsnotify.Event) {
 		log.Printf("Config file changed: %s \n", e.Name)
-		if err := viper.Unmarshal(C); err != nil {
+		if err := viper.Unmarshal(&c.cfg); err != nil {
 			handleError(err, "unmarshal conf failed")
 		}
 	})
 
 	viper.WatchConfig()
+
+	return c
 }
 
-func (c *config) Merge(cfg any) error {
-	return nil
+// pathParse parses the path and returns the dir, filename and extension.
+func pathParse(path string) (string, string, string) {
+	dir := filepath.Dir(path)
+	filename := filepath.Base(path)
+	extension := filepath.Ext(filename)
+	fileType := strings.TrimPrefix(extension, ".")
+	filenameOnly := strings.TrimSuffix(filename, extension)
+
+	return dir, filenameOnly, fileType
+}
+
+// handleError handles the error.
+func handleError(err error, message string) {
+	if err != nil {
+		log.Fatal(errors.Internal(message, err.Error()))
+	}
 }
--- config/config_test.go
+++ config/config_test.go
@@ -1,6 +1,19 @@
 package config
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestConfig_Load(t *testing.T) {
+	type config struct {
+		Http struct {
+			Host string `json:"host"`
+			Port int    `json:"port"`
+		} `json:"http"`
+	}
+
+	c := new(config)
+	C.Load("../config.yaml", c)
+
+	t.Log(c)
 }
