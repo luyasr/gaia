76877c7b90bbb0218df6e567770d2ffab5f3485d
luyasanr <luyasanr@gmail.com>
2023-12-28 15:57:54 +0800

    fix

--- ioc/store.go
+++ ioc/store.go
@@ -6,7 +6,6 @@ type Container struct {
 	store map[string]Object
 }
 
-// 加载所有对象
 func (c *Container) Load() error {
 	for _, obj := range c.store {
 		obj := obj
@@ -18,7 +17,6 @@ func (c *Container) Load() error {
 	return nil
 }
 
-// 获取对象
 func (c *Container) Get(name string) Object {
 	return c.store[name]
 }
@@ -29,7 +27,6 @@ func (c *Container) Registry(object Object) {
 	}
 }
 
-// 注册所有路由
 func (c *Container) GinIRouterRegistry(r gin.IRouter) {
 	for _, obj := range c.store {
 		if router, ok := obj.(GinIRouter); ok {
