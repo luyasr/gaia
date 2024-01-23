d02f6f3ecfb39af971c1aa88a4ff24e543232895
luyasanr <luyasanr@gmail.com>
2024-01-23 22:10:27 +0800

    fix

--- stores/redis/config.go
+++ stores/redis/config.go
@@ -2,8 +2,6 @@ package redis
 
 import (
 	"fmt"
-
-	"github.com/luyasr/gaia/errors"
 )
 
 type Config struct {
@@ -12,28 +10,9 @@ type Config struct {
 	Password string `json:"password"`
 	DB       int    `json:"db" default:"0"`
 	Protocol int    `json:"protocol" default:"3"`
+	PoolSize int    `json:"pool_size"`
 }
 
 func (c *Config) Address() string {
 	return fmt.Sprintf("%s:%d", c.Host, c.Port)
-}
-
-func (c *Config) Validate() error {
-	if c.Host == "" {
-		return errors.Internal("redis", "host is empty")
-	}
-
-	if c.Port == 0 {
-		return errors.Internal("redis", "port is empty")
-	}
-
-	if c.Password == "" {
-		return errors.Internal("redis", "password is empty")
-	}
-
-	if c.Protocol < 0 || c.Protocol > 3 {
-		return errors.Internal("redis", "protocol is invalid")
-	}
-
-	return nil
 }
--- stores/redis/redis.go
+++ stores/redis/redis.go
@@ -2,88 +2,63 @@ package redis
 
 import (
 	"context"
+	"runtime"
 	"sync"
 
-	"github.com/cenkalti/backoff/v4"
-	"github.com/luyasr/gaia/log"
 	"github.com/luyasr/gaia/reflection"
 	"github.com/redis/go-redis/v9"
 )
 
+var once sync.Once
+
 type Redis struct {
 	Client *redis.Client
 	config Config
-	once sync.Once
 }
 
 type Option func(*Redis)
 
-func getClient(ctx context.Context, r *Redis) (*redis.Client, error) {
+func NewRedis(c Config, opts ...Option) (*Redis, error) {
+	reflection.SetUp(&c)
+
+	if c.PoolSize == 0 {
+		c.PoolSize = 10 * runtime.GOMAXPROCS(0)
+	}
+
+	rds, err := newRedis(c, opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return rds, nil
+}
+
+func newRedis(c Config, opts ...Option) (*Redis, error) {
 	var err error
-	r.once.Do(func() {
+
+	r := &Redis{
+		config: c,
+	}
+
+	for _, opt := range opts {
+		opt(r)
+	}
+
+	once.Do(func() {
 		r.Client = redis.NewClient(&redis.Options{
 			Addr:     r.config.Address(),
 			Password: r.config.Password,
 			DB:       r.config.DB,
 			Protocol: r.config.Protocol,
+			PoolSize: r.config.PoolSize,
 		})
 
-		_, err = r.Client.Ping(ctx).Result()
+		_, err = r.Client.Ping(context.Background()).Result()
 	})
 
-	return r.Client, err
-}
-
-func (r *Redis) Reconnect(ctx context.Context) {
-	go func() {
-		for {
-			if r.Client == nil {
-				return
-			}
-
-			operation := func() error {
-				_, err := r.Client.Ping(ctx).Result()
-				return err
-			}
-
-			// 使用 backoff 库进行指数回避重连
-			expBackOff := backoff.NewExponentialBackOff()
-			backOffCtx := backoff.WithContext(expBackOff, ctx)
-			if err := backoff.Retry(operation, backOffCtx); err != nil {
-				log.Errorf("redis reconnect failed: %v", err)
-			}
-		}
-	}()
-}
-
-func NewRedis(c Config, opts ...Option) (*Redis, error) {
-	// 设置默认值
-	reflection.SetUp(&c)
-	// 校验 redis 配置信息
-	if err := c.Validate(); err != nil {
-		return nil, err
-	}
-
-	rds := &Redis{
-		config: c,
-	}
-
-	for _, opt := range opts {
-		opt(rds)
-	}
-
-	return newRedis(context.Background(), rds)
-}
-
-func newRedis(ctx context.Context, r *Redis) (*Redis, error) {
-	var err error
-	r.Client, err = getClient(ctx, r)
 	if err != nil {
-		log.Fatalf("redis connect failed: %v", err)
 		return nil, err
 	}
-
-	r.Reconnect(ctx)
 
 	return r, nil
 }
