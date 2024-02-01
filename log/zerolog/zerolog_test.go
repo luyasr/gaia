2d549866c51f1de962276eea47e4f4e709b1cfde
luyasanr <luyasanr@gmail.com>
2024-02-01 10:02:47 +0800

    fix

--- log/zerolog/zerolog_test.go
+++ log/zerolog/zerolog_test.go
@@ -1,16 +1,28 @@
 package zerolog
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/luyasr/gaia/errors"
 	"github.com/luyasr/gaia/log"
 )
 
+type Prosen struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func (p *Prosen) String() string {
+	bytes, _ := json.Marshal(p)
+	return string(bytes)
+}
+
 func TestNew(t *testing.T) {
+	p := &Prosen{"alex", 19}
 	logger := New(NewConsoleLogger())
 	helper := log.NewHelper(logger)
-	helper.Debug("")
+	helper.Infof("hello world %s", p)
 	str := "??"
 	helper.Debugf("%s", str)
 	helper.Debugw("msg", 12345, "error", errors.Internal("login failed", "incorrect account name or password").Error())
@@ -20,8 +32,8 @@ func TestNew(t *testing.T) {
 	filterHelper.Error("hello world")
 	filterHelper.Infow("password", "12345")
 
-	l, _ := NewFileLogger(Config{Mode: ModeSize})
-	fileLogger := New(l)
-	fileHelper := log.NewHelper(fileLogger)
-	fileHelper.Debug("hello world")
+	// l, _ := NewFileLogger(Config{Mode: ModeSize})
+	// fileLogger := New(l)
+	// fileHelper := log.NewHelper(fileLogger)
+	// fileHelper.Debug("hello world")
 }
