b26b34b72807a327c520f6804595bc4f04040670
luyasanr <luyasanr@gmail.com>
2024-04-25 13:04:35 +0800

    fix

--- transport/transport.go
+++ transport/transport.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"context"
 	"net"
+	"strconv"
 )
 
 type Server interface {
@@ -13,5 +14,14 @@ type Server interface {
 func IsValidAddress(address string) bool {
 	_, port, err := net.SplitHostPort(address)
 
-	return err == nil && port != "0"
+	return err == nil && isValidPort(port)
+}
+
+func isValidPort(port string) bool {
+	portInt, err := strconv.Atoi(port)
+	if err != nil {
+		return false
+	}
+
+	return portInt > 0 && portInt <= 65535
 }
